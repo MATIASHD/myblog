f3dae5cd07679dc89985b03c59e5d42ba7ad7b14
Matias Arevalo <arevalo.matias418@gmail.com>
2024-03-07 14:03:23 -0300

    Se agregaron dependencia falta configurarlas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const server = require('./src/index');
-const PORT = process.env.PORT || 3001;
+const PORT = 3001 || process.env.PORT;
 
 server.listen(PORT, () => {
     console.log("El server esta corriendo en el puerto " + PORT);
